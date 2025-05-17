791bc5f9a432d5283ce9f5509cc1331499255851
Abhay Patidar <abhaypatidar.dev@gmail.com>
2025-05-17 23:56:01 +0530

    Add dotenv and Title in index.html

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const httpServer = createServer(app);
